659175c9062798048dd56a6a1bba9b1cd2d1e205
TomasS <43637908+GrimReaper6667@users.noreply.github.com>
2020-03-16 18:45:55 +0100

    enabled sw

--- build/sw.js
+++ build/sw.js
@@ -1,31 +1,31 @@
-// const filesToCache = [
-//     '/',
-//     '/css/style.css',
-//     '/js/index.js',
-//     '/images/the-movie-db-logo.cb5571ba.png',
-//     '/manifest-webmanifest.json',
-//     '/images/no-image.svg',
-//     '/images/icons-192.png',
-//     '/images/icons-152.png'
-// ];
+const filesToCache = [
+    '/',
+    '/css/style.css',
+    '/js/index.js',
+    '/images/the-movie-db-logo.cb5571ba.png',
+    '/manifest-webmanifest.json',
+    '/images/no-image.svg',
+    '/images/icons-192.png',
+    '/images/icons-152.png'
+];
 
 
-// self.addEventListener('install', function(e) {
-//     // console.log(e)
-//     e.waitUntil(
-//       caches.open('pwa-cache').then(function(cache) {
-//         return cache.addAll(filesToCache);
-//       })
-//     );
-//    });
+self.addEventListener('install', function(e) {
+    // console.log(e)
+    e.waitUntil(
+      caches.open('pwa-cache').then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+    );
+   });
 
 
-// self.addEventListener('fetch', function(event) {
-//     // console.log(event.request.url);
+self.addEventListener('fetch', function(event) {
+    // console.log(event.request.url);
 
-//     event.respondWith(
-//         caches.match(event.request).then(function(response) {
-//           return response || fetch(event.request);
-//         })
-//       );
-// });
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+        })
+      );
+});
