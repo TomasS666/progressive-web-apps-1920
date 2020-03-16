c4d5af19a6f41b89fb2d6a72319d1cf99d8241fe
TomasS <43637908+GrimReaper6667@users.noreply.github.com>
2020-03-16 20:47:11 +0100

    fixed img load event. Need to remove them again somehow though

--- build/js/index.js
+++ build/js/index.js
@@ -1,5 +1,22 @@
-window.onload = function() {
+imageComplete()
+
+
+function imageComplete(imgs){
     document.querySelectorAll("article.loading").forEach(node =>{
-        node.removeAttribute("class")
+
+        node.querySelector("a img").addEventListener("load",  ()=> {
+            node.classList.remove("loading") 
+            // node.querySelector("a img").removeEventListener("load", () => )
+        })
+        
     })
+}
+
+window.onload = function() {
+    // document.querySelectorAll("article.loading").forEach(node =>{
+    //     node.removeAttribute("class")
+    // })
+    
 };
+
+
